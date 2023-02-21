c9271e6f40383d83423712f32eb8a2c6ad6565ff
Kyle McNamara <kylegm3@illinois.edu>
2023-02-21 16:26:53 -0600

    added client running script and simple test

---
+++ client/shell_scripts/run_clients.sh
@@ -0,0 +1,8 @@
+#note: I put executable in shell script folder
+for i in 1 2 3
+do
+  echo "starting client $i"
+  ./shell_scripts/go_build_shared_registers_client.exe < ./shell_scripts/inputs_$i.txt > /shell_scripts/outputs_$i.txt &
+done
+echo "clients done running commands"
+sleep 20
