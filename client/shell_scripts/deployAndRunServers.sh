ecdacfe4766da25c53093ca2c7fa0f8224bafed8
Boris Li <borui2000@gmail.com>
2023-02-21 22:41:25 -0600

    add: scripts to start/stop servers

---
+++ client/shell_scripts/deployAndRunServers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for tmp in 204 200 183 185 192
+do
+  {
+    serverName="amd${tmp}.utah.cloudlab.us"
+
+    ssh "borisli@${serverName}" "kill \`lsof -i:50051 -t\`;\
+      cd shared-registers/server; git reset --hard HEAD; git pull --no-rebase;\
+      chmod +x ../shell_scripts/*;\
+      source /etc/profile
+      make clean; make build; make run"
+  }&
+done
+wait
+echo "All servers are running now."
